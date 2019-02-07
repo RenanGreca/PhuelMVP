19923c9bd6b117dafb58ddc40e15f06e95f21349
Renan Greca <renangreca@gmail.com>
2019-02-07 22:39:34 +0100

    renamed vapor folder

---
+++ vapor2/Sources/App/routes.swift
@@ -0,0 +1,26 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello there"
+    }
+    
+    router.get("graphs") { req in
+        return try req.view().render("graphs", [
+            "name": "there"//req.parameters.next(String.self)
+        ])
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
